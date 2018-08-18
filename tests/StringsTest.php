97b78f13b8229ce6a2207e7aae852b8ef4c88c2e
davidjeddy <me@davidjeddy.com>
2018-08-18 15:58:34 -0400

    FIXED failing build due to forgetting quotes on norm_str_replace() 3rd param.

--- tests/StringsTest.php
+++ tests/StringsTest.php
@@ -32,7 +32,7 @@ final class StringsTest extends TestCase
 
     public function test_norm_str_replace(): void
     {
-        $result1 = norm_str_replace($this->testString, 'mno', ABC);
+        $result1 = norm_str_replace($this->testString, 'mno', 'ABC');
         $result2 = str_replace('mno', 'ABC', $this->testString);
 
         $this->assertEquals($result1, $result2);
